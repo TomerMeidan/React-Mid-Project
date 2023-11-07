45ea22cc9bc2f4741e6b6a7877861b7ad2928771
Tomer Meidan <tomer.m@circ.zone>
2023-11-07 15:59:20 +0200

    fix: vite web hosting fixed on gh pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://tomermeidan.github.io/React-Mid-Project",
   plugins: [react()],
 })
